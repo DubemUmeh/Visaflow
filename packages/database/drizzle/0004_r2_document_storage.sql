abbc0663a371c321a9df92782009d6c7293364e3
Dubem Umeh <raphaelumeh21@gmail.com>
2026-09-09 20:37:21 +0000

    Update 0004_r2_document_storage.sql

--- packages/database/drizzle/0004_r2_document_storage.sql
+++ packages/database/drizzle/0004_r2_document_storage.sql
@@ -1,4 +1,2 @@
-ALTER TABLE "uploaded_documents" ALTER COLUMN "application_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "uploaded_documents" ADD CONSTRAINT "uploaded_documents_application_id_applications_id_fk" FOREIGN KEY ("application_id") REFERENCES "applications"("id") ON DELETE cascade;--> statement-breakpoint
 CREATE UNIQUE INDEX "uploaded_docs_storage_key_unique_idx" ON "uploaded_documents" USING btree ("storage_key");--> statement-breakpoint
 CREATE INDEX "uploaded_docs_application_type_active_idx" ON "uploaded_documents" USING btree ("application_id","document_type") WHERE "deleted_at" IS NULL;
